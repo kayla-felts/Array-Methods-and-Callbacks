1c19d6791e57d00ad0b6d4e2658f1f34b844c00a
kayla felts <kayla.thurman1127@gmail.com>
2020-09-15 18:22:10 -0400

    close

--- index.js
+++ index.js
@@ -43,13 +43,14 @@ console.log(getYears(getFinals()))
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-
-    /* code here */
-
+function getWinners(callback) {
+    const winners = callback.map(function(wins){
+    return `${wins['Win conditions']}`;
+})
+    return winners;
 };
 
-getWinners();
+console.log(getWinners(getFinals()));
 
 /* Task 5: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 
@@ -58,11 +59,18 @@ Parameters:
  * callback function getYears
  */
 
-function getWinnersByYear(/* code here */) {
-
+function getWinnersByYear(cbgetWinners,cbgetYears) {
+    const year = cbgetYears.map(function(date){
+        return date.Year;
+    });
+    const country = cbgetWinners.map(function(name){
+        return `${name['Winning condition']}`;
+    });
+    return `In ${year} 
 };
 
-getWinnersByYear();
+
+
 
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
